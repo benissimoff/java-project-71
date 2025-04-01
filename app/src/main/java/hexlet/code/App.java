029b45e415137db9a21ace7119c56a3c7b06a186
ben <186181126+benissimoff@users.noreply.github.com>
2025-04-01 17:04:00 +0300

    add format parameter

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,10 +3,17 @@ package hexlet.code;
 import picocli.CommandLine;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Parameters;
 
 @Command(name = "gendiff",
         description = "Compares two configuration files and shows a difference.")
 public class App {
+    @Option(names = {"-f", "--format"}, paramLabel="format", description = "output format [default: stylish]")
+    String format = "stylish";
+    @Parameters(index = "0", description = "path to first file", paramLabel = "filepath1")
+    String filepath1;
+    @Parameters(index = "1", description = "path to second file", paramLabel = "filepath2")
+    String filepath2;
     @Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this help message and exit.")
     boolean usageHelpRequested;
     @Option(names = {"-V", "--version"}, versionHelp = true, description = "Print version information and exit.")
