9f0ed3876fc2b505c51b982d41d27100f0f106d5
ben <186181126+benissimoff@users.noreply.github.com>
2025-04-01 16:51:36 +0300

    add makefile and help

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,17 +1,14 @@
-
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
 
 plugins {
     id("java")
     id("application")
-//    id("com.github.ben-manes.versions") // , version("$version"))
     id("com.github.ben-manes.versions") version "0.50.0"
     id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 application { mainClass.set("hexlet.code.App") }
-
 
 repositories {
     mavenCentral()
@@ -20,7 +17,13 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("info.picocli:picocli:4.7.6")
+    annotationProcessor("info.picocli:picocli-codegen:4.7.6")
 }
+
+//compileJava {
+//    options.compilerArgs += ["-Aproject=${project.group}/${project.name}"]
+//}
 
 tasks.test {
     useJUnitPlatform()
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,28 @@
 package hexlet.code;
 
+import picocli.CommandLine;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Command;
+
+@Command(name = "gendiff",
+        description = "Compares two configuration files and shows a difference.")
 public class App {
+    @Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this help message and exit.")
+    boolean usageHelpRequested;
+    @Option(names = {"-V", "--version"}, versionHelp = true, description = "Print version information and exit.")
+    boolean versionInfoRequested;
+
     public static void main(String[] args) {
+        CommandLine commandLine = new CommandLine(new App());
+        commandLine.parseArgs(args);
+        if (commandLine.isUsageHelpRequested()) {
+            commandLine.usage(System.out);
+            return;
+        } else if (commandLine.isVersionHelpRequested()) {
+            commandLine.printVersionHelp(System.out);
+            return;
+        }
+
         System.out.println("Hello World!");
     }
 }
