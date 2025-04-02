3d66e58d8b96453d77c52699958aee9a0ffcf814
ben <186181126+benissimoff@users.noreply.github.com>
2025-04-03 02:01:20 +0300

    read and parse files

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,25 +1,35 @@
 package hexlet.code;
 
+import com.fasterxml.jackson.core.ObjectCodec;
+import com.fasterxml.jackson.core.type.TypeReference;
 import picocli.CommandLine;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Parameters;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
 
 @Command(name = "gendiff",
         description = "Compares two configuration files and shows a difference.")
 public class App {
     @Option(names = {"-f", "--format"}, paramLabel="format", description = "output format [default: stylish]")
-    String format = "stylish";
+    private static String format = "stylish";
     @Parameters(index = "0", description = "path to first file", paramLabel = "filepath1")
-    String filepath1;
+    private static String filepath1;
     @Parameters(index = "1", description = "path to second file", paramLabel = "filepath2")
-    String filepath2;
+    private static String filepath2;
     @Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this help message and exit.")
-    boolean usageHelpRequested;
+    private static boolean usageHelpRequested;
     @Option(names = {"-V", "--version"}, versionHelp = true, description = "Print version information and exit.")
-    boolean versionInfoRequested;
+    private static boolean versionInfoRequested;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        // display help
         CommandLine commandLine = new CommandLine(new App());
         commandLine.parseArgs(args);
         if (commandLine.isUsageHelpRequested()) {
@@ -29,6 +39,24 @@ public class App {
             commandLine.printVersionHelp(System.out);
             return;
         }
+
+        // get file path
+        Path path1 =  Paths.get(filepath1);
+        Path path2 = Paths.get(filepath2);
+
+        System.out.println(filepath1);
+        System.out.println(filepath2);
+
+        // read file
+        File file1 = new File(filepath1);
+        File file2 = new File(filepath2);
+        // parse file to Map
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, Object> map1 = objectMapper.readValue(file1, new TypeReference<Map<String,Object>>(){});
+        Map<String, Object> map2 = objectMapper.readValue(file2, new TypeReference<Map<String,Object>>(){});
+        System.out.println(map1);
+        System.out.println(map1.get("timeout"));
+        System.out.println(map2);
 
         System.out.println("Hello World!");
     }
