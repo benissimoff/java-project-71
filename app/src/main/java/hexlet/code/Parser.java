dc3d237cae5c06d39706f876402d6dec458f076f
ben <186181126+benissimoff@users.noreply.github.com>
2025-04-15 01:58:06 +0300

    refactor: move different file format parsing to corresponding methods

--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -3,23 +3,39 @@ package hexlet.code;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
-
+import java.util.Map;
 import java.io.File;
 import java.io.IOException;
-import java.util.Map;
 
 public class Parser {
     public static Map<String, Object> parse(String filepath) throws IOException {
         File file = new File(filepath);
-
         String fileType = filepath.substring(filepath.lastIndexOf('.') + 1);
-        ObjectMapper objectMapper = new YAMLMapper();
-        if (fileType.endsWith(".json")) {
-            objectMapper = new ObjectMapper();
+        Map<String, Object> result;
+
+        switch (fileType) {
+            case "yaml":
+            case "yml":
+                result = readYaml(file);
+                break;
+            case "json":
+            default:
+                result = readJson(file);
+                break;
         }
 
-        Map<String, Object> result = objectMapper.readValue(file, new TypeReference<Map<String, Object>>() { });
-
         return result;
+    }
+
+    private static Map<String, Object> readYaml(File file) throws IOException {
+        ObjectMapper objectMapper = new YAMLMapper();
+
+        return objectMapper.readValue(file, new TypeReference<Map<String, Object>>() { });
+    }
+
+    private static Map<String, Object> readJson(File file) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        return objectMapper.readValue(file, new TypeReference<Map<String, Object>>() { });
     }
 }
